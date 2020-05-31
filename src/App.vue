870e8a975dfdefa7632578515ea46e63b52afeeb
Mykolas Mankevcius <mykolas@unity3d.com>
2020-05-31 22:38:00 +0300

    Fix tpyo

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <main class="container flex items-center justify-center min-h-screen py-10">
     <div class="text-lg">
-      <div class="rounded-lg max-w-lg overflow-hidden">
+      <div class="max-w-lg overflow-hidden rounded-lg">
         <img alt="Bambucka and me" src="./assets/bambuckaandme.png" />
       </div>
-      <h1 class="font-display text-5xl mt-4">Mykolas Mankevicius</h1>
+      <h1 class="mt-4 text-5xl font-display">Mykolas Mankevicius</h1>
       <p class="font-body">
         Hello this is my space.
       </p>
       <p>Currently working on <a class="text-teal-500 underline" href="https://octafest.com">Octafest.com</a></p>
-      <p class="mt-4">What else do?</p>
-      <ul class="list-disc ml-5">
+      <p class="mt-4">What else?</p>
+      <ul class="ml-5 list-disc">
         <li><a class="text-teal-500 underline" href="https://octo-ui.netlify.app">Octo-UI</a></li>
         <li><a class="text-teal-500 underline" href="https://octo-form.netlify.app/">Octo-Forms</a></li>
         <li><a class="text-teal-500 underline" href="https://octo-page-builder.netlify.app">Octo-Pagebuilder</a></li>
       </ul>
       <p class="mt-4">You can reach me at:</p>
-      <ul class="list-disc ml-5">
+      <ul class="ml-5 list-disc">
         <li><a class="text-teal-500 underline" href="https://twitter.com/mmmykolas">Twitter</a></li>
         <li><a class="text-teal-500 underline" href="https://www.linkedin.com/in/mykolas-mankevicius-42311093/">Linked-in</a></li>
         <li>Discord: Mykolas#0327</li>
